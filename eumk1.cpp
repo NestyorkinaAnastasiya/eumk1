dd8cead9b5473a66b54a42158f381e5ef5f81215
NestyorkinaAnastasiya <na_sty191@mail.ru>
2017-02-19 23:04:12 +0700

    optimization of eumk1

--- eumk1.cpp
+++ eumk1.cpp
@@ -6,37 +6,24 @@
 #include <functional>  
 
 using namespace std;
-struct pair_{
-	string left;
-	string right;
-};
-
-bool operator<(pair_ a, pair_ b){
-		if (a.left < b.left) return true;
-		else return false;
-}
 
 int n;
 vector <string> text;
-vector <pair_> pairs;
+vector <pair<string, string>> pairs;
 set <string> words_exclusion;
 multimap <int, string, greater<>> often_words;
 
-void input(){
+void input() {
 	ifstream file("text.txt");
 	string word;
 
-	while (!file.eof()){
-		file >> word;
+	while (file >> word)
 		text.push_back(word);
-	}
 	file.close();
 
 	file.open("word-exclusion.txt");
-	while (!file.eof()){
-		file >> word;
+	while (file >> word)
 		words_exclusion.insert(word);
-	}
 	file.close();
 
 	file.open("n.txt");
@@ -44,66 +31,58 @@ void input(){
 	file.close();
 }
 
-void create_often_words(){
+void create_often_words() {
 	map <string, int> words;
 	for (auto i : text)
 		//если слово не пренадлежит словам-исключениям
-		if(!words_exclusion.count(i)){
+		if (!words_exclusion.count(i)) {
 			//если нет ещё такого слова, добавляем в словарь
 			if (!words.count(i)) words.insert({ i, 1 });
-			else { //иначе ищем это слово в словаре 
-				auto &elem = words.find(i);
-				//и увеличиваем счётчик таких слов на единицу
-				elem->second++;
-			} 
+			//иначе ищем это слово в словаре 
+			//и увеличиваем счётчик таких слов на единицу
+			else words.find(i)->second++;
+
 		}
 	//если словарь не пуст
-	if (words.size()){
+	if (words.size()) {
 		//заполняем новый словарь, упорядоченный по количеству слов в словаре
 		//в порядке убывания, чтобы можно было выделить N наиболее 
 		//часто встречающихся слов
-		for (auto i : words) 
+		for (auto i : words)
 			often_words.insert({ i.second, i.first });
 		//очищаем память, выделенную под временный словарь
-		words.erase(words.begin(), words.end());
+		words.clear();
 	}
 }
 
-void add_pair(map <pair_, int> &a, string left, string right){
-	pair_ p;
-	p.left = left;
-	p.right = right;
+void add_pair(map <pair<string, string>, int> &a, string left, string right) {
+	pair<string, string> p;
+	p.first = left;
+	p.second = right;
 	if (!a.count(p)) a.insert({ p, 1 });
-	else {
-			auto &elem = a.find(p);
-			elem->second++;
-	}
+	else a.find(p)->second++;
 }
 
-void create_pairs(){
-	map <pair_, int> a;
-	map <int, pair_, greater<>> b;
+void create_pairs() {
+	map <pair<string, string>, int> a;
+	map <int, pair<string, string>, greater<>> b;
 	int k = 0;
 	//проходим по N часто встречающимся словам в тексте
 	for (auto i = begin(often_words); k < n; k++, i++) {
-		auto j = begin(text);
 		//проходим по всему тексту
-		while (j != end(text)){	
-			//ищем указатель на текущее часто встречающееся слово в тексте
-			for (;  j != end(text) && i->second != *j ; j++);
-
-			if (j != end(text)){ 
+		for (auto j = begin(text); j != end(text); j++)
+			if (i->second == *j)
 				//если оно является первым словом текста
 				if (j == begin(text)) {
 					//если правое от него слово не пренадлежит словам-исключениям
-					if (!words_exclusion.count(*(j+1)))
+					if (!words_exclusion.count(*(j + 1)))
 						//добавляем данную пару слов в словарь
 						add_pair(a, *j, *(j + 1));
 				}
 
 				//если оно является последним словом текста
-				else if (j == end(text)-1) {
-					if (!words_exclusion.count(*(j - 1))) 
+				else if (j == end(text) - 1) {
+					if (!words_exclusion.count(*(j - 1)))
 						add_pair(a, *(j - 1), *j);
 				}
 
@@ -112,20 +91,15 @@ void create_pairs(){
 					if (!words_exclusion.count(*(j - 1))) add_pair(a, *(j - 1), *j);
 					if (!words_exclusion.count(*(j + 1))) add_pair(a, *j, *(j + 1));
 				}
-
-				j++;
-			}
-		}
-
+	
 		//если словарь пар составлен для данного слова
-		if (a.size()){
+		if (a.size()) {
 			//упорядочиваем его по убыванию количества слов в новом словаре
 			for (auto i : a) b.insert({ i.second, i.first });
 			//добавляем самую часто встречающуюся пару в вектор таких пар
 			pairs.push_back(begin(b)->second);
 			//освобождаем память
-			a.erase(a.begin(), a.end());
-			b.erase(b.begin(), b.end());
+			a.clear();	b.clear();
 		}
 	}
 }
@@ -133,11 +107,11 @@ void create_pairs(){
 void output() {
 	ofstream file("result.txt");
 	for (auto i : pairs)
-		file << i.left << ' ' << i.right << endl;
+		file << i.first << ' ' << i.second << endl;
 	file.close();
 }
 
-void main(){
+void main() {
 	input();
 	create_often_words();
 	create_pairs();
